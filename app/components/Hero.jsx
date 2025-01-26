edfbd5bf94b69c71584c2e966bb0953c0d9f1d52
Muhammad Qasim <qasimyousaf094@gmail.com>
2025-01-26 15:59:16 +0500

    update the code of the production

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,10 +1,9 @@
 "use client"; // Ensures this component is only rendered on the client side
 
 import { motion } from "framer-motion";
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Hero() {
-
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
@@ -12,19 +11,21 @@ export default function Hero() {
       transition={{ duration: 0.8 }}
       className="text-center py-20"
     >
-      <h1 className="text-4xl font-bold mb-4 text-gray-800">AI-Powered EduNet Optmizer</h1>
+      <h1 className="text-4xl font-bold mb-4 text-gray-800">
+        AI-Powered EduNet Optmizer
+      </h1>
       <p className="text-xl mb-8 text-gray-800">
         Connecting underserved schools to the power of AI and the internet
       </p>
       <Link href="/chatbot">
-
         <button className="bg-indigo-600 text-white px-4 py-2 rounded-md">
           AI Tutor
-
         </button>
       </Link>
       <Link href="/network">
-<button className="border border-indigo-600 text-indigo-600 px-4 py-2 rounded-md mx-5 hover:bg-indigo-600 hover:text-white transition-colors duration-300">           Network  Dashboard
+        <button className="border border-indigo-600 text-indigo-600 px-4 py-2 rounded-md mx-5 hover:bg-indigo-600 hover:text-white transition-colors duration-300">
+          {" "}
+          Network Dashboard
         </button>
       </Link>
     </motion.div>
